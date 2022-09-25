13b99e72f98bad65ff9451a13e4066fc46d06137
MohammadJavadTabari <mohammadJavadtabari1024@outlook.com>
2022-09-25 13:42:48 -0400

    linear 5% done

--- Dayana python lab/0_Education/2_special_by_functionality/0_diagrammatic/linear.py
+++ Dayana python lab/0_Education/2_special_by_functionality/0_diagrammatic/linear.py
@@ -1,4 +1,5 @@
 # ----------------------------------------------------------- simple linear diagram with matplotlib.pyplot
+from audioop import lin2alaw
 import matplotlib.pyplot as plt
 
 x = [4,5,6,7,8,9,10,12,13,15]
@@ -8,3 +9,15 @@ plt.plot(x,y)
 plt.show()
 
 # ----------------------------------------------------------- simple linear diagram with matplotlib.pyplot and numpy as data provider
+import numpy as np
+
+x = np.random.randint(low=1, high=15, size=30)
+plt.plot(x, linewidth=3)
+plt.show()
+
+# ----------------------------------------------------------- sample of using plot options 
+x = [7,14,21,28,35,42,49]
+y = [8,13,21,30,31,44,50]
+# option " o-- " show plot points on chart
+plt.plot(x, y, 'o--', linewidth=2)
+plt.show()
