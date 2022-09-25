0adf2d75e7dce5ccb30db2efdc88f89ebfda72a4
MohammadJavadTabari <mohammadJavadtabari1024@outlook.com>
2022-09-25 13:08:27 -0400

    add leaniear diagram / start code by usage samples

---
+++ Dayana python lab/0_Education/2_special_by_functionality/0_diagrammatic/linear.py
@@ -0,0 +1,10 @@
+# ----------------------------------------------------------- simple linear diagram with matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+x = [4,5,6,7,8,9,10,12,13,15]
+y = [5,6,7,8,9,10,15,16,7,1]
+
+plt.plot(x,y)
+plt.show()
+
+# ----------------------------------------------------------- simple linear diagram with matplotlib.pyplot and numpy as data provider
