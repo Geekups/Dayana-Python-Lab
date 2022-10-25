666f678a95b3041d30a526b451e9955bd6bf62b2
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2022-10-25 03:01:06 -0700

    hot bugs

---
+++ Dayana python lab/2_game/1_hipy_game/part 5 - H.B/level6_are_you_ok.py
@@ -0,0 +1,11 @@
+# I hope so :)
+
+# so welcome here
+
+# you can do this level or not. that's your choice
+
+# this is your task:
+
+# descripe what is a bug, how should you behave with bug, why a bug happen's and like that
+
+# that's it, you can just write a line or write an article!
